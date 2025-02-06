97fb5b5bc9f3e3a96a603132f323475700391cf8
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-06 15:36:19 +0100

    update 'aws_lambda_trigger_operator.py' to use context['ds']

--- dags/operators/aws_lambda_trigger_operator.py
+++ dags/operators/aws_lambda_trigger_operator.py
@@ -32,48 +32,51 @@ class AwsLambdaTriggerOperator(BaseOperator):
         env_vars=None,
         invocation_type='RequestResponse',
         aws_region=None,
+        dag=None,
         *args, **kwargs
     ):
-        super().__init__(*args, **kwargs)
+        super().__init__(dag=dag, *args, **kwargs)
         self.lambda_arn = lambda_arn
         self.env_vars = env_vars or {}
         self.invocation_type = invocation_type
         self.aws_region = aws_region
 
     def execute(self, context):
+        # Log context keys for debugging purposes
+        self.log.info("Context keys: %s", list(context.keys()))
+        
         # Prepare the payload, starting with any extra environment variables passed in.
         payload = self.env_vars.copy()
         
-        # Add the logical date (use 'logical_date' if available, otherwise fall back to 'execution_date')
-        if 'logical_date' in context:
-            payload['LOGICAL_DATE'] = str(context['logical_date'])
-        elif 'execution_date' in context:
-            payload['LOGICAL_DATE'] = str(context['execution_date'])
+        # Set the current date using context['ds']
+        if 'ds' in context:
+            current_date = context['ds']
+            payload['LOGICAL_DATE'] = current_date
         else:
-            self.log.warning("No logical_date or execution_date found in context")
-
+            self.log.warning("context['ds'] not found in context. 'LOGICAL_DATE' not set.")
+        
         self.log.info("Invoking Lambda '%s' with payload: %s", self.lambda_arn, payload)
-
+        
         # Prepare boto3 client parameters
         client_kwargs = {}
         if self.aws_region:
             client_kwargs['region_name'] = self.aws_region
-
+        
         lambda_client = boto3.client('lambda', **client_kwargs)
-
+        
         # Call the Lambda function with the payload
         response = lambda_client.invoke(
             FunctionName=self.lambda_arn,
             InvocationType=self.invocation_type,
             Payload=json.dumps(payload)
         )
-
+        
         self.log.info("Lambda function invoked. Raw response: %s", response)
-
+        
         # Check for a success status code
         if response.get("StatusCode") != 200:
             raise Exception("Lambda invocation failed with status code: {}".format(response.get("StatusCode")))
-
+        
         # Read and decode the response payload to avoid returning unserializable objects.
         payload_response = response['Payload'].read().decode('utf-8')
         self.log.info("Lambda response payload: %s", payload_response)
